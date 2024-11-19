a21921d0e8079fe96fbdc77d288c21409d34b327
Ian Israel <ian@brandboom.com>
2024-11-19 11:05:24 -0800

    added new groups

--- cypress/e2e/group3/test14.cy.js
+++ cypress/e2e/group3/test14.cy.js
@@ -1,6 +1,9 @@
 
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    it('passes', () => {
+        // generate a random number between 30000 and 240000
+        const randomNumber = Math.floor(Math.random() * (240000 - 30000 + 1)) + 30000;
+        cy.visit('https://example.cypress.io');
+        cy.wait(randomNumber);
+    });
+});
